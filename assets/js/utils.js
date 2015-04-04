4ef03f1bccd52650e33a7715f8165438dda0ea61
Saleh Saiid <saleh.saiid@gmail.com>
2015-04-04 20:57:59 +0300

    add categories list on tablet screen

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -4,7 +4,7 @@ utils = (function(){
   return {
     initOwlCarousel : function($selector, custom){
       var defaults = {
-        items : 4,
+        items : 3,
         navigation : false,
         pagination: false,
         autoPlay: true,
